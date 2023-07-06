bf34b4bf46c9ceb14fac7e2425f7220a204b805d
ErinNPeters <erin.nicole.peters@gmail.com>
2023-07-06 09:27:05 -0500

    ef core code first database created

---
+++ cookingWithPots/cookingWithPots/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cookingWithPots.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base() { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Ingredient> Ingredients => Set<Ingredient>();
+        public DbSet<Instruction> Instructions => Set<Instruction>();
+        public DbSet<Recipe> Recipes => Set<Recipe>();
+    }
+}
