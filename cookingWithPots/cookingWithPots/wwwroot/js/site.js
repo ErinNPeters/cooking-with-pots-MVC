3c656933fd7eb23dc092204badc972905f409e19
ErinNPeters <erin.nicole.peters@gmail.com>
2023-07-06 17:15:27 -0500

    Rough start to recipe of the day partial. Scaffolding for recipe views and queries.

--- cookingWithPots/cookingWithPots/wwwroot/js/site.js
+++ cookingWithPots/cookingWithPots/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(getRecipeOfTheDay);
+
+function getRecipeOfTheDay(){
+    $.ajax({
+        url: "/Recipe/RecipeOfTheDay",
+        success: function(view) {
+            $("#RecipeOfTheDay").html(view);
+        }
+    });
+}
