15a86d7cac6cf7983ecdfde624e1a108a37d7495
ErinNPeters <erin.nicole.peters@gmail.com>
2023-07-10 09:05:12 -0500

    View and Edit buttons navigate from home page

--- cookingWithPots/cookingWithPots/wwwroot/js/Index.js
+++ cookingWithPots/cookingWithPots/wwwroot/js/Index.js
@@ -7,6 +7,18 @@ function getRecipeOfTheDay() {
         url: "/Recipe/RecipeOfTheDay",
         success: function (view) {
             $("#RecipeOfTheDay").html(view);
+            $("#EditBtn").on("click", goToEdit);
+            $("#ViewBtn").on("click", goToView);
         }
     });
+}
+
+function goToEdit() {
+    var url = 'Recipes/Edit/' + $('#RecipeId')[0].value;
+    window.location.href = url;
+}
+
+function goToView() {
+    var url = 'Recipes/Details/' + $('#RecipeId')[0].value;
+    window.location.href = url;
 }
